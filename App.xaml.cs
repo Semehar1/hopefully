5dacafe712f22a84133e4b11c933758607ecad2f
Semehar Mebrahtu Hailu <semeharhailu@gmail.com>
2019-10-21 06:10:02 -0700

    first commit

---
+++ App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+namespace hopefully
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new hopefully.Loginpage();//here we make our login page as the main page
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ Loginpage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace hopefully
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Loginpage : ContentPage
+    {
+        public Loginpage()
+        {
+            InitializeComponent();
+        }
+        private void submit_Clicked(object sender, EventArgs e)
+        {
+            //here we get the value of both entries
+            string name = username.Text;
+            string pass = password.Text;
+            //here we show the value of both variable in popup
+            DisplayAlert("Entries value", name+ " " + pass, "ok");
+
+
+        }
+    }
+}
